cd7a1ce71d7055b76f757d08bc8a7fba7b23bd1a
dantohe <dantohe@gmail.com>
2015-06-08 21:13:00 -0600

    major changes in the findPatientsIDs.rb - almost 100% re written; changes to the yml to point to the Amzon AWS hosted Cypress

--- findPatientsIDs.rb
+++ findPatientsIDs.rb
@@ -6,315 +6,195 @@ require 'zip/zipfilesystem'
 require 'fileutils'
 require 'health-data-standards'
 
-#a simple patient from QRDA class - consider for 
-# TODO either make it reach or remove it
+#a simple patient from QRDA - contains the source files name and the parsed record associated
 class PatientFromQRDA
-	attr_accessor :fileName
-	def initialize(fileName)
+	attr_accessor :fileName, :patient
+	def initialize(fileName, patient)
 		@fileName=fileName
+		@patient=patient
 	end
 end
 
 
 
 
-#this was intended as the main processor 
-class CypressPatientFinder
+# the main class 
+class MRNfinder
 	
 	attr_accessor :testQRDAFile, :masterQRDAFile, :file
 
 	#constructor
-	def initialize (testQRDAFile,masterQRDAFile, file)
+	def initialize (testQRDAFile)
 		@testQRDAFile=testQRDAFile
-		@masterQRDAFile=masterQRDAFile
-		@file=file
 	end
 
 
 	# loads the patients (records) from the archived QRDA file 
-	def processPatients
+	def findOriginalMRNForTests
+		
 		testPatients=Utils::collectQRDAPatientsFromZip(testQRDAFile)
-		puts "Loaded #{testPatients.length} testPatients\n\n"
-		iterateThroughRecordsAndWriteQueries( testPatients)
+		#the master records are loaded from Cypress production 
+		masterRecords=Utils::getMasterRecordsFromCypress
+
+		puts "\nLoaded #{testPatients.length} test patients"
+		puts "Loaded #{masterRecords.length} master patients"
+		puts "Processing...."
+		iterateThroughRecordsAndWriteQueries(masterRecords, testPatients)
 	end
 
+	#find matches for test patients
+	def iterateThroughRecordsAndWriteQueries(masterRecords, testPatients)
 
-	#goes through each test record/patient and creates a Mongoid query
-	# FIXME find the proper way to inject strings into Mongoid queries
-	def iterateThroughRecordsAndWriteQueries( testPatients)
-		puts "Creating a query for each test patinet - each query will identify mongo records that satisfy the section components criteria
-Using
-		allergies
-		conditions
-		encounters
-		medications
-		procedures
-		insurance_providers \n\n"
-		for patient in testPatients
-			firstName=patient.fileName.split("_")[1]
-			lastName=patient.fileName.split("_")[2].split(".")[0]
-			createQueryForPatient(firstName,lastName,file, patient.fileName)
+		#will store the results
+		testToMasterHash= Hash.new
+
+		for testRecord in testPatients
+			#iterate through master records and find candidates 
+			candidates= Utils::iterateThroughMasterRecordsAndFindCandidates(masterRecords, testRecord.patient)
+			#resolve cadidates to unique MRNs
+			Utils::getUniqueMedicalRecordNumber(candidates,testRecord,testToMasterHash)
+		end
+
+		#print results
+		testToMasterHash.each_pair do |key, value|
+			puts "#{key}\t\t\t#{value}"
 		end
 	end
 
-		
 
-	#creates query per patient
-	def createQueryForPatient(firstName,lastName,file,theFileName)
-
-		#look for this record in mongo using first name and last name 
-		patient = Record.where(first:firstName,  last:lastName).first
-
-		finalQuery=Utils::getCodesQueryForPatient(patient)
-
-
-		# write the freaking script until figuring out Mongoid sh.
-		file.write("gotIt =Record.where(#{finalQuery})\n") 
-		file.write("counter=counter+1 if gotIt.length>1 \n")
-		file.write("certain[\"#{theFileName}\"]=gotIt.first.medical_record_number if gotIt.length==1 \n")
-		#retrive the mrns
-		file.write("if gotIt.length>1\n")
-		file.write("	mrns=Array.new\n")
-		file.write("	for result in gotIt\n")
-		file.write("		if defined? result.medical_record_number\n")
-		file.write("			mrns<<result.medical_record_number\n")
-		file.write("		end\n")
-		file.write("	end\n")
-		file.write("	unCertain[\"#{theFileName}\"]=mrns\n")
-		file.write("end\n")
-
-	end	
 end
 
 
 # a collection of utilities grouped together under the same roof
 class Utils
 
+	def self.getUniqueMedicalRecordNumber(candidates,testRecord,testToMasterHash)
+
+
+		candidateIDs=Utils::collectMedicalRecordNumbers(candidates)
+
+		if(candidateIDs.length==1)
+			testToMasterHash[testRecord.fileName]=candidateIDs.first			
+		elsif((candidateIDs.length==2) & (candidateIDs & ["ee85c89f24946e2ddca12c6edc5181dc", "eb6af018e11320f5c163624beb83767d"]).present?)
+			testToMasterHash[testRecord.fileName]="ee85c89f24946e2ddca12c6edc5181dc"			
+		elsif((candidateIDs.length==4) & (candidateIDs & ["7a86cc6d87cb84461ea190e7c706d81b", "1be81ce59da982792026cc82d95bc10e","511b530c8662f8df97eb97b3eefa0618","258ee9087c5a5fe359ceb3aafff0dd76"]).present?)
+			testToMasterHash[testRecord.fileName]="258ee9087c5a5fe359ceb3aafff0dd76"			
+		elsif((candidateIDs.length==4) & (candidateIDs & ["0a26a978d07240b0f917e96727db31d8","ee85c89f24946e2ddca12c6edc5181dc","343731b99c6bdb35a1fc31ce9ed6f889","eb6af018e11320f5c163624beb83767d"]).present?)
+			testToMasterHash[testRecord.fileName]="0a26a978d07240b0f917e96727db31d8"	
+		elsif((candidateIDs.length==2) & (candidateIDs & ["929dd1f2c4c54c024fd4d18b1307fdb1", "29cecb1da08efd331fce823e12b607d5"]).present?)
+			testToMasterHash[testRecord.fileName]="929dd1f2c4c54c024fd4d18b1307fdb1"	
+		elsif((candidateIDs.length==2) & (candidateIDs & ["5f118631f09abdbdeb1962dc28bfeb27", "a6300c43651965991a4308ffaeb5381d"]).present?)
+			testToMasterHash[testRecord.fileName]="5f118631f09abdbdeb1962dc28bfeb27"	
+		elsif((candidateIDs.length==2) & (candidateIDs & ["7a86cc6d87cb84461ea190e7c706d81b", "258ee9087c5a5fe359ceb3aafff0dd76"]).present?)
+			testToMasterHash[testRecord.fileName]="258ee9087c5a5fe359ceb3aafff0dd76"	
+		elsif((candidateIDs.length>8) & (candidateIDs & ["fed089904c10b81c036adddedddebe7b", "846c1c2ba8370c2f5504b315cc4b1d5d", "b5633133e3421216ced2bdef4dbf382d", "0a26a978d07240b0f917e96727db31d8","ee85c89f24946e2ddca12c6edc5181dc","b54a4e3ab37de7e5f8094793afb8a699","2678a4e396aaec03b860d5aeadcad8e6","929dd1f2c4c54c024fd4d18b1307fdb1","fdea0c22270417d9e59f20b07f642679","91bd37f9cebf7b6ef9f72d7fd6148a81","0085074bb549ffefffa6e16ff34df140","343731b99c6bdb35a1fc31ce9ed6f889","eb6af018e11320f5c163624beb83767d","470f57b022eaeffd4d599078e851a56d","0dbaf9336f7aa1590265250a0eebe548","29cecb1da08efd331fce823e12b607d5","d156a6d38e10efc30eda3cace7456537","8130b2ff5774f1593c86eba8dca4c37b","697e147f076648275e518e7b3ff41dcd"]).present?)
+			testToMasterHash[testRecord.fileName]="fed089904c10b81c036adddedddebe7b"	
+		else
+			for v in candidateIDs
+				puts "\t#{v}"
+			end 
+		end
+	end	
+
+	#extracts the ids from a set of records 
+	def self.collectMedicalRecordNumbers(candidates)
+		ids=Array.new
+		for candidate in candidates
+			ids <<candidate.medical_record_number
+		end
+		ids
+	end
+
+	#a utility that iterates through a set of master records to find possible matches (candidates) for a test cord
+	def self.iterateThroughMasterRecordsAndFindCandidates(masterRecords,testRecord)
+		candidates=Array.new 
+		for masterRecord in masterRecords
+			#simple conditions that will reduce the record pool
+			if(masterRecord.gender==testRecord.gender && masterRecord.ethnicity[:code]==testRecord.ethnicity[:code] && masterRecord.race[:code] == testRecord.race[:code])
+				#go inside the sections and collect codes 
+				allergiesMatch=Utils::matchCodes(testRecord.allergies,masterRecord.allergies)
+				care_goalsMatch=Utils::matchCodes(testRecord.care_goals,masterRecord.care_goals)
+				conditionsMatch=Utils::matchCodes(testRecord.conditions,masterRecord.conditions)
+				encountersMatch=Utils::matchCodes(testRecord.encounters,masterRecord.encounters)
+				immunizationsMatch=Utils::matchCodes(testRecord.immunizations,masterRecord.immunizations)
+				medical_equipmentMatch=Utils::matchCodes(testRecord.medical_equipment,masterRecord.medical_equipment)
+				medicationsMatch=Utils::matchCodes(testRecord.medications,masterRecord.medications)
+				proceduresMatch=Utils::matchCodes(testRecord.procedures,masterRecord.procedures)
+				insurance_providersMatch=Utils::matchCodes(testRecord.insurance_providers,masterRecord.insurance_providers)
+
+				#create candidates based on matching codes 
+				if(allergiesMatch && care_goalsMatch && conditionsMatch && encountersMatch &&  immunizationsMatch && medical_equipmentMatch &&  medicationsMatch && proceduresMatch &&  insurance_providersMatch)
+					candidates<<masterRecord
+				end
+			end	
+		end	
+		candidates
+	end
+
 	#a utility that loads into mongoDB records from a zip containing QRDA files 
-	#it can be added to a utility class
 	def self.collectQRDAPatientsFromZip(zipFile)
-		puts "Collecting and storing reords from: #{zipFile}"
 		patientsFromQRDAs = Array.new
 		zipFilex = open(zipFile)
 		Zip::ZipFile.open(zipFilex.path) do |zipfile|
 			zipfile.entries.each do |entry|
 				data =zipfile.read(entry.name)
-				patientFromQRDA = PatientFromQRDA.new(entry.name)
+				
 				doc = Nokogiri::XML(data)
 				doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
 				doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
 				patient_data = HealthDataStandards::Import::Cat1::PatientImporter.instance.parse_cat1(doc)
 				patient = Record.update_or_create(patient_data)
+				patientFromQRDA = PatientFromQRDA.new(entry.name, patient)
 				patientsFromQRDAs << patientFromQRDA
 			end
 		end
 		patientsFromQRDAs
 	end
 
-	#formulates Mongoid queries using codes from diffrent sections of the patient record
-	# FIXME this is a mess - the section codes collection should be using one sinle method - to much code repetion here
-	def self.getCodesQueryForPatient(patient)
-				# Record::Sections.each do |section|
-		# 	puts "section #{section}"
-		# 		# puts "#{section["allergies"].codes}"
-		# 		# section.each do |entry|
-		# 		# 	puts "#{entry}"
-		# 		# end
-		# end
-		# section allergies - done
-		# section care_goals -done 
-		# section conditions - done 
-		# section encounters - done 
-		# section immunizations -done
-		# section medical_equipment  - done
-		# section medications - done
-		# section procedures - done 
-		# section results - done 
-		# section social_history
-		# section vital_signs
-		# section support
-		# section advance_directives
-		# section insurance_providers - done 
-		# section functional_statuses
-		
-		# collecting codes part
-		resultsQueryString=""
-		# patient.results.each do |enct|
-		# 	enct.codes.each_pair do |code_set, coded_values|
-		# 		coded_values.each do |coded_value|
-		# 			resultsQueryString<<"\"vital_signs.codes.#{code_set}\" => '#{coded_value}', "
-		# 		end
-		# 	end
-		# end
-
-
-
-
-
-		care_goalsQueryString=""
-		patient.care_goals.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					care_goalsQueryString<<"\"care_goals.codes.#{code_set}\" => '#{coded_value}', "
-				end
+	def self.createCodeHash(thisEntry, patientCodesHash)
+		thisEntry.codes.each_pair do |code_set, coded_values|
+			coded_values.each do |coded_value|
+				patientCodesHash[coded_value]=code_set
 			end
 		end
-
-		medical_equipmentsQueryString=""
-		patient.medical_equipment.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					medical_equipmentsQueryString<<"\"medical_equipment.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-
-		immunizationsQueryString=""
-		patient.immunizations.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					immunizationsQueryString<<"\"immunizations.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-		encountersQueryString=""
-		patient.encounters.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					encountersQueryString<<"\"encounters.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-
-		conditionsQueryString=""
-		patient.conditions.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					conditionsQueryString<<"\"conditions.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-		# puts "query for conditions: <#{conditionsQueryString}>"
-
-		insurance_providersQueryString=""
-		patient.insurance_providers.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					insurance_providersQueryString<<"\"insurance_providers.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-		# puts "query for insurance_providers: <#{insurance_providersQueryString}>"
-
-		proceduresQueryString=""
-		patient.procedures.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					proceduresQueryString<<"\"procedures.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-
-
-		medicationsQueryString=""
-		patient.medications.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					medicationsQueryString<<"\"medications.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-		# puts "query for medications: <#{medicationsQueryString}>"
-
-		allergiesQueryString=""
-		patient.allergies.each do |enct|
-			enct.codes.each_pair do |code_set, coded_values|
-				coded_values.each do |coded_value|
-					allergiesQueryString<<"\"allergies.codes.#{code_set}\" => '#{coded_value}', "
-				end
-			end
-		end
-		raceQueryString="\"race.code\"=>'#{patient.race["code"]}',"
-		seed=":last.in => [ /\\b[A-Z]\\b/] , "
-		finalResult=seed+raceQueryString+resultsQueryString+medical_equipmentsQueryString+immunizationsQueryString+care_goalsQueryString+encountersQueryString+conditionsQueryString+insurance_providersQueryString+proceduresQueryString+medicationsQueryString+allergiesQueryString
-		finalResult=finalResult.chomp(', ')
-		finalResult
 	end
 
-	#creates a script to be populated and run at the end 
-	def self.initiateTemporaryScript
-		file = File.open("autoScript.rb", "w")
-		file.write("def callAll\n") 
+	def self.matchCodes(patientEntries,masterPatientEntries)
+		result = false
 
-		file.write("Mongoid.load!(\"mongoid.yml\", :production)\n")
-		file.write("counter=0\n")
-		file.write("certain=Hash.new\n")
-		file.write("unCertain=Hash.new\n")
-		file
-	end	
+		patientCodesHash= Hash.new 
+		patientEntries.each do |thisEntry|
+			createCodeHash(thisEntry, patientCodesHash)
+		end
 
+		masterCodesHash= Hash.new 
+		masterPatientEntries.each do |thisEntry|
+			createCodeHash(thisEntry, masterCodesHash)
+		end
 
-	#this is a method that writes to the temporary scrit file
-	def self.writeFinalPartOfTheAutoGeneratedScript(file)
+		# puts"PAT"
+		# patientCodesHash.each_pair do |key, value|
+		# 	puts "\t#{key}\t#{value}"
+		# end
+		# puts"MASTER"
+		# masterCodesHash.each_pair do |key, value|
+		# 	puts "\t#{key}\t#{value}"
+		# end
 
+		if (patientCodesHash.to_a - masterCodesHash.to_a).empty?
+			result=true
+		end
+		result		
+	end
 
-		file.write("puts \"TOTAL PROCESSED ::::: \#\{certain.length+unCertain.length\}\"\n")
-		file.write("puts \"CERTAIN ::::: \#\{certain.length\}\"\n")
-		file.write("puts \"UNCERTAIN ::::: \#\{unCertain.length\}\"\n")
-
-
-
-		file.write("certain.each_pair do |key, value|\n")
-		file.write("	puts \"\#\{key\}\"\n")
-		file.write("	puts \"\#\{value\}\"\n")
-		file.write("end\n")
-
-
-		file.write("unCertain.each_pair do |key, value|\n")
-		file.write("	puts \"\#\{key\}\"\n")
-
-		#there are 6 cases with are too ambiguous - human intervention is necessary
-
-		file.write("		if((value.length==2) & (value & [\"ee85c89f24946e2ddca12c6edc5181dc\", \"eb6af018e11320f5c163624beb83767d\"]).present?)\n")
-		file.write("			puts \"! ee85c89f24946e2ddca12c6edc5181dc \"\n")
-
-		file.write("		elsif((value.length==4) & (value & [\"7a86cc6d87cb84461ea190e7c706d81b\", \"1be81ce59da982792026cc82d95bc10e\",\"511b530c8662f8df97eb97b3eefa0618\",\"258ee9087c5a5fe359ceb3aafff0dd76\"]).present?)\n")
-		file.write("			puts \"! 258ee9087c5a5fe359ceb3aafff0dd76 \"\n")
-
-		file.write("		elsif((value.length==4) & (value & [\"0a26a978d07240b0f917e96727db31d8\",\"ee85c89f24946e2ddca12c6edc5181dc\",\"343731b99c6bdb35a1fc31ce9ed6f889\",\"eb6af018e11320f5c163624beb83767d\"]).present?)\n")
-		file.write("			puts \"! 0a26a978d07240b0f917e96727db31d8 \"\n")
-
-		file.write("		elsif((value.length==2) & (value & [\"929dd1f2c4c54c024fd4d18b1307fdb1\", \"29cecb1da08efd331fce823e12b607d5\"]).present?)\n")
-		file.write("			puts \"! 929dd1f2c4c54c024fd4d18b1307fdb1 \"\n")
-
-		file.write("		elsif((value.length==2) & (value & [\"5f118631f09abdbdeb1962dc28bfeb27\", \"a6300c43651965991a4308ffaeb5381d\"]).present?)\n")
-		file.write("			puts \"! 5f118631f09abdbdeb1962dc28bfeb27 \"\n")
-
-		file.write("		elsif((value.length>8) & (value & [\"fed089904c10b81c036adddedddebe7b\", \"846c1c2ba8370c2f5504b315cc4b1d5d\", \"b5633133e3421216ced2bdef4dbf382d\", \"0a26a978d07240b0f917e96727db31d8\",\"ee85c89f24946e2ddca12c6edc5181dc\",\"b54a4e3ab37de7e5f8094793afb8a699\",\"2678a4e396aaec03b860d5aeadcad8e6\",\"929dd1f2c4c54c024fd4d18b1307fdb1\",\"fdea0c22270417d9e59f20b07f642679\",\"91bd37f9cebf7b6ef9f72d7fd6148a81\",\"0085074bb549ffefffa6e16ff34df140\",\"343731b99c6bdb35a1fc31ce9ed6f889\",\"eb6af018e11320f5c163624beb83767d\",\"470f57b022eaeffd4d599078e851a56d\",\"0dbaf9336f7aa1590265250a0eebe548\",\"29cecb1da08efd331fce823e12b607d5\",\"d156a6d38e10efc30eda3cace7456537\",\"8130b2ff5774f1593c86eba8dca4c37b\",\"697e147f076648275e518e7b3ff41dcd\"]).present?)\n")
-		file.write("			puts \"! fed089904c10b81c036adddedddebe7b \"\n")
-
-
-		file.write("		else\n")
-
-		file.write("			for v in value\n")
-		file.write("				puts \"\\t\#\{v\}\"\n")
-		file.write("			end \n")
-		file.write("		end\n")
-		file.write("end\n")
-
-
-		file.write("end\n") 
-		file.close unless file.nil?
+	#collects records from a Cypress mongo db
+	def self.getMasterRecordsFromCypress
+		Mongoid.load!("mongoid.yml", :production)
+		masterRecords = Record.all_of(:last.in => [ /\b[A-Z]\b/])
+		masterRecords
 	end
 end
 
-
-# look here into the options to add optparse
-# if(ARGV.length<2)
-# 	puts "Two arguments are needed: \n\t1. path to the zip containing the QRDA1 test files 
-# 	2. path to the zip containing the master QRDA1 files."
-# 	abort
-# end
 if(ARGV.length<1)
 	puts "One argument are needed: \n\t1. path to the zip containing the QRDA1 test files"
 	abort
@@ -323,21 +203,8 @@ end
 
 
 Mongoid.load!("mongoid.yml", :development)
-
-#due to a mongoid issues (cannot make it call a query build out of a string) - so I create a temporary script that I call after 
-scriptFile=Utils::initiateTemporaryScript
-
-#create a patient finder 
-patientFinder = CypressPatientFinder.new(ARGV[0], ARGV[1],scriptFile)
-patientFinder.processPatients
-
-
-#writes the final part that creates the output
-Utils::writeFinalPartOfTheAutoGeneratedScript(scriptFile)
-require_relative 'autoScript'
-
-puts "PRINTING RESULTS"
-
-#call the auto generated script
-callAll
+#instantiate with an collection archived collection of QRDA1 files
+patientFinder = MRNfinder.new(ARGV[0])
+#process the records and retrive original MRNs
+patientFinder.findOriginalMRNForTests
 
